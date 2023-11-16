d7eeec07276ff06e2078ac63f65970c6603e7279
Alexander Kopetsch <akopetsch92@gmail.com>
2023-11-16 20:24:03 +0100

    IndicesChunk.cs: add Olganix reference

--- SWE1R.Assets.Blocks/ModelBlock/Meshes/VertexIndices/IndicesChunk.cs
+++ SWE1R.Assets.Blocks/ModelBlock/Meshes/VertexIndices/IndicesChunk.cs
@@ -6,6 +6,9 @@ using ByteSerialization.Attributes;
 
 namespace SWE1R.Assets.Blocks.ModelBlock.Meshes.VertexIndices
 {
+    /// <summary>
+    /// <see href="https://github.com/akopetsch/Sw_Racer/blob/76c8ad9cea549ea18457846a135a7f25d48b3813/include/Swr_Model.h#L515">SWR_MODEL_Section48</see>
+    /// </summary>
     [Sizeof(8)]
     public abstract class IndicesChunk
     {
