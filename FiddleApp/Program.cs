6b342ece060d36470df439670cf1f16c8f11dffa
Alexander Kopetsch <akopetsch92@gmail.com>
2023-11-25 18:38:02 +0100

    FiddleApp

---
+++ FiddleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2023 SWE1R.Assets Maintainers
+// Licensed under GPLv2 or any later version
+// Refer to the included LICENSE.txt file.
+
+using SWE1R.Assets.Blocks.SpriteBlock;
+using SWE1R.Assets.Blocks.Utils;
+using System.Diagnostics;
+
+namespace FiddleApp
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            int result = Fiddle();
+            if (Debugger.IsAttached)
+                ConsoleUtils.PromptExit();
+            return result;
+
+        }
+
+        private static int Fiddle()
+        {
+            //new RoslynCodeGenerationExample();
+            new ByteSerializerSqlFiddle(typeof(Sprite));
+            return ExitCodes.Success;
+        }
+    }
+}
---
+++ FiddleApp/TypeKeywordMapper.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2023 SWE1R.Assets Maintainers
+// Licensed under GPLv2 or any later version
+// Refer to the included LICENSE.txt file.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FiddleApp
+{
+    public static class TypeKeywordMapper
+    {
+        public static string GetKeywordFromType(Type type)
+        {
+            // Create a small piece of code
+            var code = $"var exampleVar = default({type.FullName});";
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+            // Set up a compilation
+            var compilation = CSharpCompilation.Create("TypeKeywordMapping")
+                .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+                .AddSyntaxTrees(syntaxTree);
+
+            // Get the semantic model
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+
+            // Find the variable declaration
+            var variableDeclaration = syntaxTree.GetRoot().DescendantNodes()
+                .OfType<VariableDeclarationSyntax>()
+                .FirstOrDefault();
+
+            // Get the type info
+            var typeInfo = semanticModel.GetTypeInfo(variableDeclaration.Type);
+
+            // Get the special type and map it to the keyword
+            return typeInfo.Type.ToDisplayString();
+        }
+    }
+}
