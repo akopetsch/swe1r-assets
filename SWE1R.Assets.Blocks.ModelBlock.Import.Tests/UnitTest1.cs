41702b8467fe54fa2e95a20974ca6384b0ab10be
Alexander Kopetsch <akopetsch92@gmail.com>
2023-12-09 21:43:28 +0100

    SWE1R.Assets.Blocks.ModelBlock.Import.Tests

---
+++ SWE1R.Assets.Blocks.ModelBlock.Import.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+// Copyright 2023 SWE1R.Assets Maintainers
+// Licensed under GPLv2 or any later version
+// Refer to the included LICENSE.txt file.
+
+namespace SWE1R.Assets.Blocks.ModelBlock.Import.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
