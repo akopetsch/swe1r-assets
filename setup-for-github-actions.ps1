7b69cfc6ef798a353acaec877ec602d6fa10222b
Alexander Kopetsch <akopetsch92@gmail.com>
2024-05-27 15:42:55 +0200

    nuget: setup-for-github-actions.ps1, global.json

---
+++ setup-for-github-actions.ps1
@@ -0,0 +1,4 @@
+# Create a global.json file to specify the required .NET SDK version.
+# This file is used by GitHub Actions to install the required .NET SDK version.
+# https://www.meziantou.net/publishing-a-nuget-package-following-best-practices-using-github.htm
+dotnet new globaljson --roll-forward feature --force
