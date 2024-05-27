5587d323cfdca70ebf807265957b2359b0b90f23
Alexander Kopetsch <akopetsch92@gmail.com>
2024-05-27 17:08:13 +0200

    GitHub Action: add publish.yml

--- setup-for-github-actions.ps1
+++ setup-for-github-actions.ps1
@@ -1,4 +1,5 @@
 # Create a global.json file to specify the required .NET SDK version.
 # This file is used by GitHub Actions to install the required .NET SDK version.
+# inspired by:
 # https://www.meziantou.net/publishing-a-nuget-package-following-best-practices-using-github.htm
 dotnet new globaljson --roll-forward feature --force
