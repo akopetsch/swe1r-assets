1899bfef763a8ee2f3c1692bb0e301c4ee0d9314
Alexander Kopetsch <akopetsch92@gmail.com>
2024-05-27 15:21:02 +0200

    clean-up: update source file header (copyright year)

--- SWE1R.Assets.Blocks.ModelBlock.Import.Resources.Tests/GlobalUsings.cs
+++ SWE1R.Assets.Blocks.ModelBlock.Import.Resources.Tests/GlobalUsings.cs
@@ -1,4 +1,4 @@
-// Copyright 2023 SWE1R.Assets Maintainers
+// Copyright 2024 SWE1R.Assets Maintainers
 // Licensed under GPLv2 or any later version
 // Refer to the included LICENSE.txt file.
 
--- SWE1R.Assets.Blocks.ModelBlock.Import.Resources/Resources/BlenderSuzanne/BlenderSuzanneResourcesHelper.cs
+++ SWE1R.Assets.Blocks.ModelBlock.Import.Resources/Resources/BlenderSuzanne/BlenderSuzanneResourcesHelper.cs
@@ -1,6 +1,6 @@
-﻿// Copyright 2023 SWE1R.Assets Maintainers
+﻿// Copyright 2024 SWE1R.Assets Maintainers
 // Licensed under GPLv2 or any later version
-// Refer to the included LICENSE.txt file
+// Refer to the included LICENSE.txt file.
 
 using SWE1R.Assets.Blocks.Utils;
 
