d1527067e78ca62ac87b82d97a11368b59eb593d
Alexander Kopetsch <akopetsch92@gmail.com>
2024-06-06 01:01:44 +0200

    MaterialTexture: re-format, reference SW_RACER_RE C struct member names

--- src/SWE1R.Assets.Blocks/ModelBlock/Materials/MaterialTexture.cs
+++ src/SWE1R.Assets.Blocks/ModelBlock/Materials/MaterialTexture.cs
@@ -31,64 +31,98 @@ namespace SWE1R.Assets.Blocks.ModelBlock.Materials
         #region Properties (serialized)
 
         /// <summary>
+        /// <para>SW_RACER_RE: unk0</para>
         /// Always 0, 1, 0x41 (0100 0001) or 0x49 (0100 1001). 
         /// Strong positive correllation with <see cref="Word_0e">Word_0e</see>.
-        /// Strong positive correlation with the number of elements in <see cref="Children">Children</see> 
-        /// that are not null.
+        /// Strong positive correlation with the number of elements in <see cref="Children">Children</see> that are not null.
         /// </summary>
-        [Order(0)] public int Mask_Unk { get; set; }
+        [Order(0)]
+        public int Mask_Unk { get; set; }
         /// <summary>
+        /// <para>SW_RACER_RE: res[0]</para>
         /// Always four times <see cref="Width">Width</see>.
         /// </summary>
-        [Order(1)] public short Width4 { get; set; }
+        [Order(1)]
+        public short Width4 { get; set; }
         /// <summary>
+        /// <para>SW_RACER_RE: res[1]</para>
         /// Always four times <see cref="Height">Height</see>.
         /// </summary>
-        [Order(2)] public short Height4 { get; set; }
+        [Order(2)]
+        public short Height4 { get; set; }
         /// <summary>
+        /// <para>SW_RACER_RE: unk1[0]</para>
         /// Always zero.
         /// </summary>
-        [Order(3)] public short Always0_08 { get; set; } = 0;
+        [Order(3)]
+        public short Always0_08 { get; set; } = 0;
         /// <summary>
+        /// <para>SW_RACER_RE: unk1[1]</para>
         /// Always zero.
         /// </summary>
-        [Order(4)] public short Always0_0a { get; set; } = 0;
-        [Order(5)] public TextureFormat Format { get; set; }
+        [Order(4)]
+        public short Always0_0a { get; set; } = 0;
         /// <summary>
+        /// <para>SW_RACER_RE: type</para>
+        /// </summary>
+        [Order(5)]
+        public TextureFormat Format { get; set; }
+        /// <summary>
+        /// <para>SW_RACER_RE: num_children</para>
         /// Most of the time 0 (6383 times). Sometimes 4 (28 times). 
         /// Strong positive correlation with <see cref="Mask_Unk">Mask_Unk</see>. 
         /// Very strong positive correlation with the number of elements in <see cref="Children">Children</see> that are not null.
         /// </summary>
-        [Order(6)] public short Word_0e { get; set; }
-        [Order(7)] public short Width { get; set; }
-        [Order(8)] public short Height { get; set; }
+        [Order(6)]
+        public short Word_0e { get; set; }
         /// <summary>
+        /// <para>SW_RACER_RE: width</para>
+        /// </summary>
+        [Order(7)]
+        public short Width { get; set; }
+        /// <summary>
+        /// <para>SW_RACER_RE: height</para>
+        /// </summary>
+        [Order(8)]
+        public short Height { get; set; }
+        /// <summary>
+        /// <para>SW_RACER_RE: unk2</para>
         /// Always 128, 256 or 512 times <see cref="Width">Width</see>.
         /// </summary>
-        [Order(9)] public ushort Width_Unk { get; set; }
+        [Order(9)]
+        public ushort Width_Unk { get; set; }
         /// <summary>
+        /// <para>SW_RACER_RE: unk3</para>
         /// Always 128, 256 or 512 times <see cref="Height">Height</see>.
         /// </summary>
-        [Order(10)] public ushort Height_Unk { get; set; }
+        [Order(10)]
+        public ushort Height_Unk { get; set; }
         /// <summary>
+        /// <para>SW_RACER_RE: unk4</para>
         /// Original values are in <see cref="OriginalFlagsValues">OriginalFlagsValues</see>.
         /// </summary>
-        [Order(11)] public short Flags { get; set; }
+        [Order(11)]
+        public short Flags { get; set; }
         /// <summary>
+        /// <para>SW_RACER_RE: unk5</para>
         /// Strong negative correlation with 
         /// <see cref="Mask_Unk">Mask_Unk</see> and 
-        /// <see cref="Word_0e">Word_0e</see>.
+        /// <see cref="Word_0e">Word_0e</see>. 
         /// Significant negative correlation with the number of elements in <see cref="Children">Children</see> that are not null.
         /// Original values are in <see cref="OriginalMaskValues">OriginalMaskValues</see>.
         /// </summary>
-        [Order(12)] public short Mask { get; set; }
-
-        [Length(ChildrenCount)]
-        [ElementReference]
-        [Order(13)] public MaterialTextureChild[] Children { get; set; }
-
-        [Offset(0x38)]
-        [Order(14)] public TextureIndex TextureIndex { get; set; }
+        [Order(12)]
+        public short Mask { get; set; }
+        /// <summary>
+        /// <para>SW_RACER_RE: specs</para>
+        /// </summary>
+        [Order(13), Length(ChildrenCount), ElementReference]
+        public MaterialTextureChild[] Children { get; set; } = new MaterialTextureChild[ChildrenCount];
+        /// <summary>
+        /// <para>SW_RACER_RE: texture_index</para>
+        /// </summary>
+        [Order(14), Offset(0x38)]
+        public TextureIndex TextureIndex { get; set; }
 
         #endregion
 
@@ -130,17 +164,12 @@ namespace SWE1R.Assets.Blocks.ModelBlock.Materials
 
         #endregion
 
-        #region Constructor
-
-        public MaterialTexture() =>
-            Children = new MaterialTextureChild[ChildrenCount];
-
-        #endregion
-
         #region Methods (: object)
 
         public override string ToString() =>
-            $"({nameof(Width)}={Width}, {nameof(Height)}={Height}, {nameof(TextureIndex)}={TextureIndex})";
+            $"({nameof(Width)}={Width}, " +
+            $"{nameof(Height)}={Height}, " +
+            $"{nameof(TextureIndex)}={TextureIndex})";
 
         #endregion
     }
