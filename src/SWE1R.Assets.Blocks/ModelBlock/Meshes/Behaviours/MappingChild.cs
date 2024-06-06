2a665a8b4a4127f2f7fd3b2f6ef8de6cde3a64dd
Alexander Kopetsch <akopetsch92@gmail.com>
2024-06-06 14:17:05 +0200

    MappingChild: re-format

--- src/SWE1R.Assets.Blocks/ModelBlock/Meshes/Behaviours/MappingChild.cs
+++ src/SWE1R.Assets.Blocks/ModelBlock/Meshes/Behaviours/MappingChild.cs
@@ -29,46 +29,50 @@ namespace SWE1R.Assets.Blocks.ModelBlock.Meshes.Behaviours
         /// <summary>
         /// Seems to be a position vector.
         /// </summary>
-        [Order(0)] public Vector3Single Vector_00 { get; set; }
-
+        [Order(0)]
+        public Vector3Single Vector_00 { get; set; }
         /// <summary>
         /// X: -1.0 - 1.0,  two decimal places
         /// Y: -1.0 - 1.0,  two decimal places
         /// Z: -0.3 - 0.11, two decimal places
         /// </summary>
-        [Order(1)] public Vector3Single Vector_0c { get; set; }
-        [Order(2)] public short Word_18 { get; set; }
-        [Order(3)] public byte Byte_1a { get; set; }
-        [Order(4)] public byte Byte_1b { get; set; }
-        [Order(5)] public short Word_1c { get; set; }
-
+        [Order(1)]
+        public Vector3Single Vector_0c { get; set; }
+        [Order(2)]
+        public short Word_18 { get; set; }
+        [Order(3)]
+        public byte Byte_1a { get; set; }
+        [Order(4)]
+        public byte Byte_1b { get; set; }
+        [Order(5)]
+        public short Word_1c { get; set; }
         /// <summary>
         /// 53 different values, 59 times 163
         /// </summary>
-        [Order(6)] public byte Byte_1e { get; set; }
-
+        [Order(6)]
+        public byte Byte_1e { get; set; }
         /// <summary>
         /// 25 different values, 59 times 31, 59 times 215
         /// </summary>
-        [Order(7)] public byte Byte_1f { get; set; }
-
+        [Order(7)]
+        public byte Byte_1f { get; set; }
         /// <summary>
         /// Nullable.
         /// </summary>
-        [Reference(ReferenceHandling.Postpone)]
-        [Order(8)] public FlaggedNode FlaggedNode_20 { get; set; }
-        [Order(9)] public short Word_24 { get; set; }
-
+        [Order(8), Reference(ReferenceHandling.Postpone)]
+        public FlaggedNode FlaggedNode_20 { get; set; }
+        [Order(9)]
+        public short Word_24 { get; set; }
         /// <summary>
         /// Padding. (always 0)
         /// </summary>
-        [Order(10)] public short Word_26 { get; set; }
-
+        [Order(10)]
+        public short Word_26 { get; set; }
         /// <summary>
         /// Null if this is the last one.
         /// </summary>
-        [Reference]
-        [Order(11)] public MappingChild Next { get; set; }
+        [Order(11), Reference]
+        public MappingChild Next { get; set; }
 
         #endregion
     }
