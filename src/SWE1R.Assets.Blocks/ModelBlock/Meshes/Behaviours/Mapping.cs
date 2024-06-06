93690921dacfba7b46af585d8f0d4417538dd6e1
Alexander Kopetsch <akopetsch92@gmail.com>
2024-06-06 14:13:32 +0200

    Mapping: re-format

--- src/SWE1R.Assets.Blocks/ModelBlock/Meshes/Behaviours/Mapping.cs
+++ src/SWE1R.Assets.Blocks/ModelBlock/Meshes/Behaviours/Mapping.cs
@@ -25,34 +25,40 @@ namespace SWE1R.Assets.Blocks.ModelBlock.Meshes.Behaviours
     {
         #region Properties (serialized)
 
-        [Order(0)] public short Word_00 { get; set; }
-
-        [Order(1)] public byte FogFlags { get; set; }
-        [Order(2)] public Vector3Byte FogColor { get; set; }
-        [Order(3)] public ushort FogStart { get; set; }
-        [Order(4)] public ushort FogEnd { get; set; }
-
-        [Order(5)] public ushort LightFlags { get; set; }
-
-        [Order(6)] public Vector3Byte AmbientColor { get; set; }
-        [Order(7)] public Vector3Byte LightColor { get; set; }
-
-        [Order(8)] public byte Byte_12 { get; set; }
-        [Order(9)] public byte Byte_13 { get; set; }
-
-        [Order(10)] public Vector3Single LightVector { get; set; }
-
-        [Order(11)] public float Float_20 { get; set; }
-        [Order(12)] public float Float_24 { get; set; }
-
-        [Offset(0x2c)]
-        [Order(13)] public VehicleReaction VehicleReaction { get; set; }
-
-        [Order(14)] public short Word_30 { get; set; }
-        [Order(15)] public short Word_32 { get; set; }
-
-        [Length(typeof(SubLengthHelper))]
-        [Order(16)] public List<MappingSub> Subs { get; set; }
+        [Order(0)]
+        public short Word_00 { get; set; }
+        [Order(1)]
+        public byte FogFlags { get; set; }
+        [Order(2)]
+        public Vector3Byte FogColor { get; set; }
+        [Order(3)]
+        public ushort FogStart { get; set; }
+        [Order(4)]
+        public ushort FogEnd { get; set; }
+        [Order(5)]
+        public ushort LightFlags { get; set; }
+        [Order(6)]
+        public Vector3Byte AmbientColor { get; set; }
+        [Order(7)]
+        public Vector3Byte LightColor { get; set; }
+        [Order(8)]
+        public byte Byte_12 { get; set; }
+        [Order(9)]
+        public byte Byte_13 { get; set; }
+        [Order(10)]
+        public Vector3Single LightVector { get; set; }
+        [Order(11)]
+        public float Float_20 { get; set; }
+        [Order(12)]
+        public float Float_24 { get; set; }
+        [Order(13), Offset(0x2c)]
+        public VehicleReaction VehicleReaction { get; set; }
+        [Order(14)]
+        public short Word_30 { get; set; }
+        [Order(15)]
+        public short Word_32 { get; set; }
+        [Order(16), Length(typeof(SubLengthHelper))]
+        public List<MappingSub> Subs { get; set; }
 
         #endregion
 
